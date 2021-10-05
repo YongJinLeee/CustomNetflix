e74db22cf9acd02fe684502ef87b6857f5d11685
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-10-06 01:10:54 +0900

    Feat : SearchViewController 파일 생성 및 Storyboard 연결

---
+++ CustomNetflix/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  CustomNetflix
+//
+//  Created by LeeYongJin on 2021/10/06.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
