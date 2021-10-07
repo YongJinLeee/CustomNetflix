cd37746943a6b3b24fc23f42b45afe3c6afd83c6
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-10-08 01:16:50 +0900

    Feat : Codable protocol에 맞춰 구조체 코딩
    - Movie, Response 구조체의 stored property 작성
    - JSON 파싱을 위한 CodingKey 열거형 property  작성

--- CustomNetflix/SearchViewController.swift
+++ CustomNetflix/SearchViewController.swift
@@ -27,7 +27,6 @@ class SearchViewController: UIViewController {
 }
 
 extension SearchViewController: UISearchBarDelegate {
-    
     // 키보드 내리기
     private func dismissKeyboard() {
         searchBar.resignFirstResponder()
@@ -47,7 +46,6 @@ extension SearchViewController: UISearchBarDelegate {
         searchAPI.search(searchTerm) {
             movies in
         }
-        
         print("search Bar clicked: \(searchTerm) 에 대한 검색이 시작되었습니다.")
         // 옵셔널 바인딩 되었으므로 콘솔 확인 searchBar.text -> searchTerm으로 변경
     }
@@ -101,9 +99,25 @@ class searchAPI {
 // model
 
 struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
     
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
 }
 
-struct Response {
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
     
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
 }
