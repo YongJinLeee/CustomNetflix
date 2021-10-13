42bd2c80ff38749f81d6274cb201b431d8b6f56c
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-10-14 04:28:48 +0900

    Feat : Close 버튼클릭시 Search ViewController로 넘어가도록 구현
    - retrive된 검색 화면이 아닌 이전화면으로 다시 돌아가는 방법 찾기...

--- CustomNetflix/PlayerViewController.swift
+++ CustomNetflix/PlayerViewController.swift
@@ -8,5 +8,38 @@
 import UIKit
 
 class PlayerViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        playerClosed()
+    }
+    
+    @IBOutlet weak var closeBtn: UIButton!
+    
+    
+    @IBAction func closingPlayer(_ sender: Any) {
+        
+        print("close btn clicked")
+     
+        let SearchView =  UIStoryboard(name: "Main", bundle: nil)
+        let searchVC = SearchView.instantiateViewController(identifier: "SearchViewController") as! SearchViewController
+        searchVC.modalPresentationStyle = .fullScreen
+        present(searchVC, animated: false, completion: nil)
+    }
+    
+    
+    func playerClosed() {
+        
+        if closeBtn.isSelected == true {
+            
+        let SearchView =  UIStoryboard(name: "Main", bundle: nil)
+        let searchVC = SearchView.instantiateViewController(identifier: "SearchViewController") as! SearchViewController
+        present(searchVC, animated: false, completion: nil)
+        }
+    }
 
 }
+
+
