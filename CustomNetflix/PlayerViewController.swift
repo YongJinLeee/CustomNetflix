8fc55b5c152da5b9d5389e547dbd3dbdfa9de791
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-10-19 19:43:48 +0900

    Feat : Player Controller view의 버튼 동작 설정
    - Play-Pause 토글 구현
    - close 버튼 클릭시 세로모드로 전환하도록 UIInterfaceOrientationMask를 portrait 지정하는 함수 추가

--- CustomNetflix/PlayerViewController.swift
+++ CustomNetflix/PlayerViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class PlayerViewController: UIViewController {
     
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var controllView: UIView!
     // player 가로뷰 자동설정
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscape
@@ -20,14 +23,25 @@ class PlayerViewController: UIViewController {
     
     @IBOutlet weak var closeBtn: UIButton!
     
+    @IBAction func togglePlayBtn(_ sender: Any) {
+        
+        playBtn.isSelected = !playBtn.isSelected
+        
+    }
     @IBAction func closingPlayer(_ sender: Any) {
         
         print("클릭 확인 : close btn clicked")
         
         dismiss(animated: false, completion: nil)
+        portraitlMode()
     }
 }
 
+func portraitlMode() {
+    var supportedfaceorientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
 
 
 
