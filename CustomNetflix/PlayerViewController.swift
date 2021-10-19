c7b2cdd3d7cdf998c84b06ae78f8190f54cd0203
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-10-19 23:05:26 +0900

    Feat : 재생-일시정지 동작 상세 구현
    - Play - Pause 토글 버튼에 각각 해당 기능 구현하는 controller method 구현
    - close 버튼 클릭시 player reset 하는AVPlayerItem의 replaceCurrentItem 함수 사용

--- CustomNetflix/PlayerViewController.swift
+++ CustomNetflix/PlayerViewController.swift
@@ -23,21 +23,46 @@ class PlayerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // PlayerView 상속
+        playerView.player = player
+        
     }
     
     @IBOutlet weak var closeBtn: UIButton!
     
     @IBAction func togglePlayBtn(_ sender: Any) {
-        
-        playBtn.isSelected = !playBtn.isSelected
-        
+        if player.isPlaying { // true: 실행중
+            // 정지 -> Foundation moduel에 있음
+            pause()
+        } else {
+            play()
+            // 실행
+        }
     }
+
     @IBAction func closingPlayer(_ sender: Any) {
         
         print("클릭 확인 : close btn clicked")
-        
+        playerReset()
         dismiss(animated: false, completion: nil)
         portraitlMode()
+    }
+    
+    // player Controller method
+    func play() {
+        player.play()
+        playBtn.isSelected = true
+    }
+    
+    func pause() {
+        player.pause()
+        playBtn.isSelected = false
+    }
+    
+    func playerReset() {
+        pause()
+        player.replaceCurrentItem(with: nil)
     }
 }
 
@@ -47,6 +72,7 @@ func portraitlMode() {
     }
 }
 
+// AVPlyaer extension
 extension AVPlayer {
     
     //재생 여부 확인
