0f33107d98254bd97d7e8c61fbbffd39a889ff25
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-10-21 01:06:05 +0900

    Header에서 바로 재생할 때 필요한 URL 받아오기, Player View 띄우는 기능 구현

--- CustomNetflix/HomeViewController.swift
+++ CustomNetflix/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
 
@@ -13,7 +14,34 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // Home header 재생버튼 재생할 데이터 SearchAPI로 가져오기..
+    @IBAction func playBtnOnHomeHeader(_ sender: Any) {
+        
+        SearchAPI.search("interstella") { movieInfo in
+            guard let interstella = movieInfo.first else { return }
+            
+            DispatchQueue.main.async {
+                let MovieURL = URL(string: interstella.previewURL)!
+                let item = AVPlayerItem(url: MovieURL)
+                
+                let PlayerSB = UIStoryboard(name: "Player", bundle: nil)
+                let PlayerVC = PlayerSB.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
+                
+                PlayerVC.modalPresentationStyle = .fullScreen
+                // Player에 동영상 주소 로드
+                PlayerVC.player.replaceCurrentItem(with: item)
+                self.present(PlayerVC, animated: false, completion: nil)
+            }
+        }
+        
+    }
 }
+
+
+
+
+
+
+
 
