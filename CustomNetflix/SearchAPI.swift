96912ab034d9288e782e47f2bce0166a9d9336aa
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-10-25 19:28:25 +0900

    commentary code fixed

--- CustomNetflix/SearchAPI.swift
+++ CustomNetflix/SearchAPI.swift
@@ -2,8 +2,9 @@
 import UIKit
 
 class SearchAPI {
-    //type method - 인스턴스 생성 없이 .으로 바로 메소드 호출 가능
-    // escaping을 쓰면 해당 코드블럭 바깥에서도 사용 될 수 있음을 명시할 수 있음
+    // type method - 인스턴스 생성 없이 .으로 바로 메소드 호출 가능
+    // escaping : https://jusung.github.io/Escaping-Closure/ 참조
+    
     static func search(_ term: String, completion: @escaping (([Movie]) -> Void)) {
         let session = URLSession(configuration: .default)
         
@@ -21,7 +22,7 @@ class SearchAPI {
         let dataTask = session.dataTask(with: requestURL) { data, response, error in
             let successRange = 200..<300
             
-            // error 테스트 코드
+            // error test code
             guard error == nil, let statusCode = (response as? HTTPURLResponse)?.statusCode,
                   successRange.contains(statusCode) else {
                 print("Error Code: \(successRange)")
@@ -36,9 +37,7 @@ class SearchAPI {
             
             let parsedMoviesInfo = SearchAPI.parseMovies(resultData)
             completion(parsedMoviesInfo)
-//            print("search URL Operation Test : \(parsedMoviesInfo.count), 첫 번째 영화의 이름은 \(parsedMoviesInfo.first?.title)")
-//            completion([Movie])
-            
+
         }
         dataTask.resume()
     }
