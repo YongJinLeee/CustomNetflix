fea8577ebc17b8f43520eff8423984c8682c136b
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-10-27 18:41:26 +0900

    Podfile - firebase init

--- Pods/Target Support Files/Pods-CustomNetflix-CustomNetflixUITests/Pods-CustomNetflix-CustomNetflixUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CustomNetflix_CustomNetflixUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CustomNetflix_CustomNetflixUITests
-@end
