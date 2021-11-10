8f300fbe2044af9994b2bc6042cb8292ad065be4
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-11-11 02:21:39 +0900

    Feat : Firebace module import

--- CustomNetflix/HomeViewController.swift
+++ CustomNetflix/HomeViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import AVFoundation
+import Firebase
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
     
